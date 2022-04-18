2d41dfd61f6287d84d84c99d1ba77292484f6471
JKsofts <48759196+JKsofts@users.noreply.github.com>
2022-04-18 15:29:24 +0800

    Add context hook to handle connection state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import ChatForm from './components/ChatForm.js';
+import JoinForm from './components/JoinForm.js';
 import MainChat from './components/MainChat.js';
+import ChatContext from "./components/ChatContext.js";
+
+import './App.css';
+
+
 
 function App() {
+
+  const [isConnected, setIsConnected] = useState(false);
+  const [members, setMembers] = useState([]);
+
   return (
     <div >
-      <h1>Mini-chat</h1>
-      <MainChat />
+      <h1>Mini-Chat</h1>
+        <ChatContext.Provider value={{connectState: [isConnected, setIsConnected], memberList:[members, setMembers]}}>
+          {!isConnected? 
+          <JoinForm /> :
+          <><MainChat /><ChatForm /></>}
+        </ChatContext.Provider>
     </div>
   );
 }
--- src/components/MainChat.js
+++ src/components/MainChat.js
@@ -1,10 +1,6 @@
 // import io from 'socket.io-client';
 /* eslint-disable no-undef */
 
-import ChatForm from "./ChatForm.js";
-import LoginForm from "./LoginForm.js";
-/* eslint-disable no-undef */
-
 
 import "../index.css"
 
@@ -13,11 +9,9 @@ export default function MainChat() {
 
   return (
     <div>
-      <LoginForm/>
       <div className="chat-box">
         <ul></ul>
       </div>
-      <ChatForm />
     </div>
   );
 }
